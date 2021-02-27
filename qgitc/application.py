2823b065b1e217af9004027a3b98fb558678347e
Weitian Leung <weitianleung@gmail.com>
2021-02-27 22:18:12 +0800

    Switch to PySide6

--- qgitc/application.py
+++ qgitc/application.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-from PySide2.QtWidgets import QApplication
-from PySide2.QtGui import QIcon, QDesktopServices
-from PySide2.QtCore import (
+from PySide6.QtWidgets import QApplication
+from PySide6.QtGui import QIcon, QDesktopServices
+from PySide6.QtCore import (
     Qt,
     QTranslator,
     QLibraryInfo,
